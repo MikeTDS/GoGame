34dbe914f71c4e558910e3358486dd097ff2f3f8
MikeTDS <m_ilski@interia.pl>
2020-01-08 19:29:31 +0100

    +poprawne matchdata

--- src/main/java/goGame/DataBase/MatchData.java
+++ src/main/java/goGame/DataBase/MatchData.java
@@ -1,14 +1,37 @@
 package goGame.DataBase;
 
 public class MatchData {
-    StringBuilder data;
-    public MatchData(String game){
-        addMove(game + "\n");
+    Boolean finished;
+    Boolean withBot;
+    int id;
+    String name;
+    StringBuilder whiteMoves;
+    StringBuilder blackMoves;
+    int[] blockedField = {-1,-1};
+    public MatchData(String game, Boolean withBot){
+        this.name = game;
+        this.withBot=withBot;
+        finished=false;
+        whiteMoves = new StringBuilder();
+        blackMoves = new StringBuilder();
     }
-    public void addMove(String move){
-        data.append(move);
+    public void addMove(String color, int x, int y){
+        if(color.equalsIgnoreCase("white"))
+            whiteMoves.append(x + ";" + y + "\n");
+        else if(color.equalsIgnoreCase("black"))
+            blackMoves.append(x + ";" + y + "\n");
     }
-    public String setDataToSend(){
-        return data.toString();
+    public void setBlockedField(int x, int y){
+        blockedField[0]=x;
+        blockedField[1]=y;
+    }
+    public void setStatusToFinished(){
+        finished=true;
+    }
+    public String setWhiteDataToSend(){
+        return whiteMoves.toString();
+    }
+    public String setBlackDataToSend(){
+        return blackMoves.toString();
     }
 }
